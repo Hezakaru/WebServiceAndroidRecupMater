42fc91559b941a1e5edcdcd389bef54009a7f394
Hezakaru <martin.delemarre@outlook.com>
2014-03-21 12:17:15 +0100

    ss
    ss

--- src/sio2/ppe/mission6/recupmater4android/Affichage.java
+++ src/sio2/ppe/mission6/recupmater4android/Affichage.java
@@ -1,8 +1,20 @@
 package sio2.ppe.mission6.recupmater4android;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Affichage extends Activity {
 
@@ -19,5 +31,23 @@ public class Affichage extends Activity {
         getMenuInflater().inflate(R.menu.affichage, menu);
         return true;
     }
+    
+    	TextView response;
+    	String uri="";
+        HttpGet get = new HttpGet();
+        get.setURI(uri);
+        DefaultHttpClient httpClient =   new DefaultHttpClient();
+        HttpResponse httpResponse = httpClient.execute(get);
+        if (httpResponse.getStatusLine().getStatusCode() == 200) {
+            Log.d("[GET REQUEST]", "HTTP Get succeeded");
+
+            HttpEntity messageEntity = httpResponse.getEntity();
+            InputStream is = messageEntity.getContent();
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            String line;
+            while ((line = br.readLine()) != null) {
+               	response.append(line);
+            }
+    
     
 }
